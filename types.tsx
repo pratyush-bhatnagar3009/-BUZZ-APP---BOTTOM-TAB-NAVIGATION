4151e21765779b917b7a5bff1b92fc8c219eda78
pratyush-bhatnagar3009 <64519337+pratyush-bhatnagar3009@users.noreply.github.com>
2020-11-23 20:31:18 +0530

    Add files via upload

---
+++ types.tsx
@@ -0,0 +1,17 @@
+export type RootStackParamList = {
+  Root: undefined;
+  NotFound: undefined;
+};
+
+export type BottomTabParamList = {
+  FaceBook: undefined;
+  Instagram: undefined;
+};
+
+export type FaceBookParamList = {
+  FaceBookScreen: undefined;
+};
+
+export type TabTwoParamList = {
+  InstagramScreen: undefined;
+};
